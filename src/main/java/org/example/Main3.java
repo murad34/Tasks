76d2bbdd2bea54417569b1cd7f09cb83673fbf21
murad34 <muradmehdiyev.az@gmail.com>
2024-06-07 18:02:04 +0400

    commit 3

---
+++ src/main/java/org/example/Main3.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main3 {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 20; i++) {
+            System.out.println(i);
+        }
+
+    }
+
+}
